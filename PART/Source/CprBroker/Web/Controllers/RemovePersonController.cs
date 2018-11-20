ef84289b75b924ba172eac3074c20429fdc13041
Heini Leander Ovason <heini.ovason@gmail.com>
2018-11-20 09:33:39 +0100

    RemovePErson controller and view added. changes added accordingly to project- and sitemap files

---
+++ PART/Source/CprBroker/Web/Controllers/RemovePersonController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CprBroker.Web.Controllers
+{
+    [RoutePrefix("Pages/RemovePerson")]
+    public class RemovePersonController : Controller
+    {
+        // GET: RemovePerson
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
