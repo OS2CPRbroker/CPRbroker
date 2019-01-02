2e1f29e521bb5e4c1c998bc001973573eb2754ff
Heini Leander Ovason <heini.ovason@gmail.com>
2019-01-02 16:06:15 +0100

    Remove Person frontend with controller response implemented.

--- PART/Source/CprBroker/Web/Controllers/RemovePersonController.cs
+++ PART/Source/CprBroker/Web/Controllers/RemovePersonController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,11 +10,38 @@ namespace CprBroker.Web.Controllers
     [RoutePrefix("Pages/RemovePerson")]
     public class RemovePersonController : Controller
     {
-        // GET: RemovePerson
+        // HTTP GET
         [Route("")]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [Route("")]
+        public ActionResult RemovePerson()
+        {
+            string response = null;
+            try
+            {
+                string cprnr = Request["cprNrInputField"].ToString();
+                if (Regex.IsMatch(cprnr, @"^\d{10}$")) {
+                    // 1) Call Part.GetUuid()
+                    // 2) Verify that everything went well.
+                    // 3) Call Admin.RemovePerson()
+                    // 4) Verify that everything went well.
+                    response = string.Format("{0} was succesfully delivered to RemovePersonController.", cprnr);
+                }
+                else
+                {
+                    response = "Please enter a valid 10 digit number.";
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                response = ex.ToString(); // Remove and log exception to CPR Broker before release.
+            }             
+            return Content(response);
         }
     }
 }
