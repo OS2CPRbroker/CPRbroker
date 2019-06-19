2739a4bc721b44df47edd70001db04d8b29786c3
Jakob <jakob@magenta.dk>
2019-06-19 13:51:34 +0200

    First attempt at putting subscription on persons moving to a new municipality

--- PART/Source/CprBroker/CPRDirect/Queues/EnsureSubscriptionQueue.cs
+++ PART/Source/CprBroker/CPRDirect/Queues/EnsureSubscriptionQueue.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CprBroker.Engine;
+using CprBroker.Schemas.Part;
+using CprBroker.PartInterface;
 
 namespace CprBroker.Providers.CPRDirect
 {
@@ -28,24 +31,47 @@ namespace CprBroker.Providers.CPRDirect
             using (var dataContext = new ExtractDataContext())
             {
                 items.LoadExtractAndItems(dataContext);
-
-                // TODO: Get municipalities in subscription from config.
+                
+                int[] subbedMunicipalities = PartInterface.Tracking.SettingsUtilities.ExcludedMunicipalityCodes;
+                HashSet<String> personsToSubscribe = new HashSet<string>();
 
                 foreach (var person in items)
                 {
-                    
                     var response = Extract.ToIndividualResponseType(person.Extract, person.ExtractItems.AsQueryable(), Constants.DataObjectMap);
                     decimal currentMunCode = response.CurrentAddressInformation.MunicipalityCode;
-                    // TODO: Check if currentMunCode is in subscribed municipalities, if it is we dont need to check historical adresses
-
-                    // If currentMunCode is not in subscribed municipalities, but latestMunCode is, then we need to subscribe to the CPR number
-
+                    
                     HistoricalAddressType identity = new HistoricalAddressType()
                     {
                         LeavingDate = DateTime.MinValue,
                         MunicipalityCode = 0
                     };
                     decimal latestMunCode = response.HistoricalAddress.Aggregate((a, b) => Newest(a, b)).MunicipalityCode;
+                    // If currentMunCode is not in subscribed municipalities, but latestMunCode is, then we need to subscribe to the CPR number
+                    // TODO: make sure newest historical address is not current address
+                    if (Array.Exists<int>(subbedMunicipalities, (a) => a == latestMunCode) 
+                        && !Array.Exists<int>(subbedMunicipalities, (a) => a == currentMunCode))
+                    {
+                        personsToSubscribe.Add(person.PNR);
+                    }
+                }
+
+                BrokerContext bc = BrokerContext.Current;
+                PartManager pm = new PartManager();
+                // Get UUIDs
+                GetUuidArrayOutputType uuids = pm.GetUuidArray(bc.UserToken, bc.ApplicationToken, personsToSubscribe.ToArray());
+                if (!StandardReturType.IsSucceeded(uuids.StandardRetur))
+                {
+                    CprBroker.Engine.Local.Admin.LogError("There was an error getting UUIDS of persons, in EnsureSubscriptionQueue. The error was: " + uuids.StandardRetur.FejlbeskedTekst);
+                    return new ExtractQueueItem[0];
+                }
+
+                // Subscribe persons
+                Guid[] guids = uuids.UUID.Select( u => Guid.Parse(u)).ToArray();
+                BasicOutputType<bool> putsubs = pm.PutSubscription(bc.UserToken, bc.ApplicationToken, guids);
+                if (!StandardReturType.IsSucceeded(putsubs.StandardRetur))
+                {
+                    CprBroker.Engine.Local.Admin.LogError("There was an error putting subscriptions of persons, in EnsureSubscriptionQueue. The error was: " + putsubs.StandardRetur.FejlbeskedTekst);
+                    return new ExtractQueueItem[0];
                 }
             }
 
