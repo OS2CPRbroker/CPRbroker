4e90f83cb531e9abb5830ca04d24d8bfe9440b2f
heiniovason <heini.ovason@gmail.com>
2019-11-18 09:16:47 +0100

    Missing assembly reference added. Corrected DAWA assembly info, and .NET Runtime Version.

--- PART/Source/CprBroker/DAWA/Properties/AssemblyInfo.cs
+++ PART/Source/CprBroker/DAWA/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using CprBroker.Utilities;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DAWA")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Integration with DANMARKS ADRESSERS WEB API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DAWA")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCompany("Magenta ApS")]
+[assembly: AssemblyProduct("CPR Broker")]
+[assembly: AssemblyCopyright("MPL 1.1/GPL 2.0/LGPL 2.1")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(Constants.Versioning.AssemblyVersion)]
+[assembly: AssemblyFileVersion(Constants.Versioning.AssemblyFileVersion)]
